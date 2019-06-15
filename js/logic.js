c9f014d577952f001c4e16c03167f5347ef8d122
oggyst <51537934+oggyst@users.noreply.github.com>
2019-06-15 17:20:37 +0200

    Add files via upload

--- js/logic.js
+++ js/logic.js
@@ -10,14 +10,14 @@ function tempUnitChange(select) {
 
 function onTempChange(chossenTemp) {
   if (chossenTemp == "fahrenheit") {
-    $('#temp').html(Math.round(token.current.temp_f) + "&#8457; <br>Feal:<br>" + Math.round(token.current.feelslike_f) + "&#8457;");
+    $('#temp').html(Math.round(token.current.temp_f) + "&#8457; <br>Feal:&#160; " + Math.round(token.current.feelslike_f) + "&#8457;");
     for (var i = 0; i < token.forecast.forecastday.length; i++) {
       $('#maxTemp' + i).html(token.forecast.forecastday[i].day.maxtemp_f + "&#8457");
       $('#minTemp' + i).html(token.forecast.forecastday[i].day.mintemp_f + "&#8457");
       $('#avgTemp' + i).html(token.forecast.forecastday[i].day.avgtemp_f + "&#8457");
     }
   } else if (chossenTemp == "celsius") {
-    $('#temp').html(Math.round(token.current.temp_c) + "&#8451 <br>Feal:<br>" + Math.round(token.current.feelslike_c) + "&#8451");
+    $('#temp').html(Math.round(token.current.temp_c) + "&#8451 <br>Feal:&#160; " + Math.round(token.current.feelslike_c) + "&#8451");
     for (var i = 0; i < token.forecast.forecastday.length; i++) {
       $('#maxTemp' + i).html(token.forecast.forecastday[i].day.maxtemp_c + "&#8451");
       document.getElementById("maxTemp" + i).style.color = 'red';
@@ -35,7 +35,7 @@ function distUnitChange(select) {
 
 function onDistChange(chossenDist) {
   if (this.chossenDist == "km") {
-    $('#desc').html(token.current.condition.text + "<br>Wind speed:" + token.current.wind_kph + "kph <br>Humidity:" + token.current.humidity + "%<br> UV:" +
+    $('#desc').html(token.current.condition.text + "<br>Wind speed:&#160; " + token.current.wind_kph + "kph <br>Humidity:&#160; " + token.current.humidity + "%<br> UV:&#160; " +
       token.current.uv);
     for (var i = 0; i < token.forecast.forecastday.length; i++) {
       $('#maxWind' + i).html(token.forecast.forecastday[i].day.maxwind_kph + "kph");
@@ -43,7 +43,7 @@ function onDistChange(chossenDist) {
       $('#avgVis' + i).html(token.forecast.forecastday[i].day.avgvis_km + "km");
     }
   } else if (this.chossenDist == "m") {
-    $('#desc').html(token.current.condition.text + "<br>Wind speed:" + token.current.wind_mph + "mph <br>Humidity:" + token.current.humidity + "%<br> UV:" +
+    $('#desc').html(token.current.condition.text + "<br>Wind speed: &#160; " + token.current.wind_mph + "mph <br>Humidity:&#160; " + token.current.humidity + "%<br> UV:&#160; " +
       token.current.uv);
     for (var i = 0; i < token.forecast.forecastday.length; i++) {
       $('#maxWind' + i).html(token.forecast.forecastday[i].day.maxwind_mph + "mph");
@@ -67,36 +67,29 @@ $(document).ready(function() {
 });
 
 function onLoad() {
-  fetch('http://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input + '&days=7')
+  fetch('https://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input + '&days=7')
     .then((res) => {
-      console.log('http://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input + '&days=7');
+      console.log('https://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input + '&days=7');
       if (res.status === 400) {
         $('#city').html("Incorrect city name <br>Displaying " + token.location.name);
       }
       return res.json();
     })
     .then(json => {
-      $.getJSON('http://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input, function(data) {
+      $.getJSON('https://api.apixu.com/v1/forecast.json?key=b9cfa296afcb457f8cb143741190706&days=7&q=' + input, function(data) {
         console.log(data);
         token = data;
-        $('#temp').html(Math.round(token.current.temp_c) + "&#8451 <br>Feal:<br>" + Math.round(token.current.feelslike_c) + "&#8451");
-				// $('descText').html(token.current.condition.text)
-        $('#desc').html("Wind speed:" + token.current.wind_kph + "kph <br>Humidity:" + token.current.humidity + "%<br> UV:" +
-          token.current.uv);
         $('#city').html(token.location.name);
-        $('#icon').attr('src', "http:" + token.current.condition.icon);
-        $('#temp').html(Math.round(token.current.temp_c) + "&#8451 <br>Feal:<br>" + Math.round(token.current.feelslike_c) + "&#8451");
-        $('#desc').html(token.current.condition.text + "<br>Wind speed:" + token.current.wind_kph + "kph <br>Humidity:" + token.current.humidity + "%<br> UV:" +
-          token.current.uv);
+        $('#icon').attr('src', "https:" + token.current.condition.icon);
         for (var i = 0; i < data.forecast.forecastday.length; i++) {
           console.log(i);
           $('#date' + i).html(token.forecast.forecastday[i].date);
-          $('#icon' + i).html("<img style = 'width:65%;height:65%;resize: none;' src=http:" + token.forecast.forecastday[i].day.condition.icon + ">");
+          $('#icon' + i).html("<img style = 'width:50px;height:50px;resize: none;' src=https:" + token.forecast.forecastday[i].day.condition.icon + ">");
           $('#condition' + i).html(token.forecast.forecastday[i].day.condition.text);
           date = (data.forecast.forecastday[i].date)
-          $("#date" + i).html(getDayOfWeek(date));
-          $('#totalPrecip' + i).html(token.forecast.forecastday[i].day.totalprecip_mm + "mm");
-          $('#avgVis' + i).html(token.forecast.forecastday[i].day.avgvis_km + "km");
+          $("#date" + i).html("<b>" + getDayOfWeek(date) + "</b> </br>" + date);
+          // $('#totalPrecip' + i).html(token.forecast.forecastday[i].day.totalprecip_mm + "mm");
+          // $('#avgVis' + i).html(token.forecast.forecastday[i].day.avgvis_km + "km");
           $('#avgHumidity' + i).html(token.forecast.forecastday[i].day.avghumidity + "%");
           onDistChange(chossenDist);
           onTempChange(chossenTemp);
